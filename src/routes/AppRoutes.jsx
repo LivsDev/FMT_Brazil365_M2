e13759fc91a736f703bbd08a932dbc252aac7264
LivsDev <liviasc.1985@gmail.com>
2024-09-14 14:17:46 -0300

    Ajuste de importação

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Login from '../pages/Login';
-import Cadastro from '../pages/CadastroUsuario';
+import CadastroUsuario from '../pages/CadastroUsuario';
 
 function AppRoutes() {
     return (
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/cadastro" element={<CadastroUsuario />} />
        
       </Routes>
     );
