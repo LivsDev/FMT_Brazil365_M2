55a97998f41b6d593fd888a206f47beeaa439ac2
LivsDev <liviasc.1985@gmail.com>
2024-09-14 14:31:02 -0300

    importação bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
