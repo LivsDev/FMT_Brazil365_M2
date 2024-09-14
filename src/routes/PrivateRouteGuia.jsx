7f4145f8acc84e23594383d0917e1a581ded58eb
LivsDev <liviasc.1985@gmail.com>
2024-09-14 16:30:22 -0300

    Rota Privada de Guia

---
+++ src/routes/PrivateRouteGuia.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext'; // Importa o contexto de autenticação
+
+function PrivateRouteGuia({ children }) {
+    const { usuarioLogado } = useAuth(); // Obtém o usuário logado
+  
+    if (!usuarioLogado) {
+      // Se não estiver logado, redireciona para o login
+      return <Navigate to="/login" />;
+    }
+  
+    if (usuarioLogado.tipoUsuario !== 'guia') {
+      // Se não for guia, redireciona para a página inicial ou exibe uma mensagem de acesso negado
+      return <Navigate to="/" />;
+    }
+  
+    // Se estiver logado e for guia, renderiza o componente filho
+    return children;
+  }
+  
+  export default PrivateRouteGuia;
