b13261bddfbe9d1101dc57ca2e6897f30ce89536
LivsDev <liviasc.1985@gmail.com>
2024-09-14 16:28:26 -0300

    Atualização de rota

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,13 +1,25 @@
 import { Routes, Route } from 'react-router-dom';
 import Login from '../pages/Login';
 import CadastroUsuario from '../pages/CadastroUsuario';
+import DashboardGuia from '../pages/DashboardGuia'; // Importa o DashboardGuia
+import PrivateRouteGuia from './PrivateRouteGuia'; // Importa a rota privada para guias
+//
 
 function AppRoutes() {
     return (
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<CadastroUsuario />} />
-       
+
+        {/* Rota protegida para o Dashboard do Guia */}
+        <Route
+        path="/dashboard-guia"
+        element={
+          <PrivateRouteGuia>
+            <DashboardGuia />
+          </PrivateRouteGuia>
+        }
+      />
       </Routes>
     );
   }
